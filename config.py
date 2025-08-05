0f3be28323c82a339d7c4b513ca0ba5a2a80957b
karta3100510-star <karta3100510@gmail.com>
2025-08-06 00:44:28 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET       = os.getenv("LINE_CHANNEL_SECRET")
+USER_ID                   = os.getenv("USER_ID")
