fa637e846e7db346ef44269a4bea19912f6716ae
karta3100510-star <karta3100510@gmail.com>
2025-08-07 21:11:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+# Ensure environment variables are set in your deployment platform (e.g., Render dashboard)
+LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
