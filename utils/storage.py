266d257f63728a747699a7147a26e31fb521c394
karta3100510-star <karta3100510@gmail.com>
2025-08-19 20:17:08 +0800

    Update storage.py

--- utils/storage.py
+++ utils/storage.py
@@ -1,15 +1,9 @@
 import os, json
-
-DATA_FILE = os.path.join('data', 'posts.json')
-
 def load_posts():
-    if not os.path.exists(DATA_FILE):
-        return []
-    with open(DATA_FILE, 'r') as f:
-        return json.load(f)
+    if not os.path.exists("data/posts.json"): return []
+    with open("data/posts.json", "r") as f: return json.load(f)
 
-def save_posts(new_posts):
+def save_posts(new_data):
     posts = load_posts()
-    posts.extend(new_posts)
-    with open(DATA_FILE, 'w') as f:
-        json.dump(posts, f, ensure_ascii=False, indent=2)
+    posts.extend(new_data)
+    with open("data/posts.json", "w") as f: json.dump(posts, f, ensure_ascii=False, indent=2)
