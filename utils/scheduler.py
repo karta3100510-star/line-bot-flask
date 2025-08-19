844ffdbd0b48f4bc0d865fdb01d8181c1ea045e6
karta3100510-star <karta3100510@gmail.com>
2025-08-19 22:57:19 +0800

    Update scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,7 +1,9 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from utils.social_crawler import crawl_social_data
 
-def init_scheduler():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(crawl_social_data, 'interval', hours=1)
-    scheduler.start()
+scheduler = BackgroundScheduler()
+
+@scheduler.scheduled_job("interval", hours=1)
+def analyze_data():
+    print("[排程任務] 每小時執行抓取社群資料")
+    crawl_social_data()
