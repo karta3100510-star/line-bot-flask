e31999cbeb6d1ffa2ecbc96da52516069c4123b4
karta3100510-star <karta3100510@gmail.com>
2025-08-19 20:15:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
+LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
 
-# Ensure environment variables are set in your deployment platform (e.g., Render dashboard)
-LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
