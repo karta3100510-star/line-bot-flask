117f24dae37219b36f153d7b1e7d0cd2f182aacb
karta3100510-star <karta3100510@gmail.com>
2025-08-19 20:18:04 +0800

    Update scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,12 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from utils.social_crawler import crawl_social_data
 from analyzer import analyze_data
 from utils.notifier import send_daily_summary
 
 scheduler = BackgroundScheduler()
-
-# Social crawl and data analysis every 1 hour
 scheduler.add_job(analyze_data, 'interval', hours=1, id='crawl_and_analyze')
-
-# Daily summary at 12:00
 scheduler.add_job(send_daily_summary, 'cron', hour=12, minute=0, id='daily_summary')
