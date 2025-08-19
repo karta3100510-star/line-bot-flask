77a296dd30f9780f0ce17f1a49dd2560476c37a4
karta3100510-star <karta3100510@gmail.com>
2025-08-19 23:19:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,15 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-
+from flask import Flask, request
 import os
-from utils.scheduler import scheduler
-from utils.social_crawler import crawl_social_data
-from utils.notifier import send_daily_summary
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text.strip().lower()
-    if msg == "/healthz":
-        reply = "LINE Bot 正常運作中"
-    elif msg == "/social":
-        crawl_social_data()
-        reply = "已執行社群抓取分析"
-    elif msg == "/summary":
-        send_daily_summary()
-        reply = "已發送摘要"
-    else:
-        reply = f"你說的是: {msg}"
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
-
 @app.route("/healthz", methods=["GET"])
 def healthz():
-    return "OK"
+    return "OK", 200
 
-# 啟動排程
-scheduler.start()
+@app.route("/", methods=["POST"])
+def webhook():
+    return "Webhook received", 200
 
 if __name__ == "__main__":
     app.run()
